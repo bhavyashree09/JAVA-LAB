3b200c43dbb00942c448294a10df51e7c422175d
bhavyashree09 <bhavyashreelaxmidevi@gmail.com>
2026-03-25 12:36:29 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+import pack.*;
+import pack2.*;
+
+public class Demo1
+{
+    public static void main(String args[])
+    {
+        Protection p = new Protection();
+        SamePackage sp = new SamePackage();
+        Derived d = new Derived();
+    }
+}
