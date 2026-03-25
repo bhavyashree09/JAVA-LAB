c4d8e2e33821ce0cc09ab178fc7b54f5dfedff92
bhavyashree09 <bhavyashreelaxmidevi@gmail.com>
2026-03-25 12:37:55 +0530

    Add files via upload

---
+++ ThreadTest.java
@@ -0,0 +1,48 @@
+class A extends Thread
+{
+    public void run()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("\t From Thread A: i="+i);
+        }
+        System.out.println("Exit from A");
+    }
+}
+
+class B extends Thread
+{
+    public void run()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("\t From Thread B: i="+i);
+        }
+        System.out.println("Exit from B");
+    }
+}
+
+class C extends Thread
+{
+    public void run()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("\t From Thread C: i="+i);
+        }
+        System.out.println("Exit from C");
+    }
+}
+
+class ThreadTest
+{
+	public static void main(String args[])
+		
+	{
+		
+		new A().start();
+		new B().start();
+		new C().start();
+}
+
+}
